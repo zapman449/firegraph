315d1dfd1119a9e9b4184b35cb0aff079e3cfa42
zapman449 <japrice@gmail.com>
2012-07-09 11:23:11 -0400

    server.py is now working in a basic way -jp

--- server.py
+++ server.py
@@ -8,8 +8,17 @@ UDP_PORT = 45454
 sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
 sock.bind( (UDP_IP, UDP_PORT) )
 
-data = {}
+results = {}
 
-while True :
-    data, addr = sock.recvfrom( 1024 )
-    data.setdefault(addr, 0) += 1
+try :
+    while True :
+        data, addr = sock.recvfrom( 1024 )
+        ip, port = addr
+        #print repr(data), repr(addr)
+        if ip not in results :
+            results[ip] = 0
+        results[ip] += 1
+except KeyboardInterrupt :
+    for k in sorted(results) :
+        print k, results[k]
+    raise
