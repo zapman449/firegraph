5ec7db557dfd988ed012059818e397124fca30b6
zapman449 <japrice@gmail.com>
2012-07-23 17:34:10 -0400

    start_all now groks the daemonized client

--- CnC/start_all.sh
+++ CnC/start_all.sh
@@ -2,6 +2,6 @@
 
 for node in 1 2 3 ; do
     for host in 0 1 2 3 4 5 6 ; do
-        ssh beacon${node}x0${host} "nohup /tmp/beacon_client.py &" &
+        ssh beacon${node}x0${host} "/tmp/beacon_client.py start" &
     done
 done
